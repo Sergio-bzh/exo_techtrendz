dff6d3ec63bb5547530c7da51a7aa229e58424d4
sergio NUNEZ <sergio.nunez@laposte.net>
2023-06-28 02:29:55 +0200

    STRUCTURE : php files added to blog structure

---
+++ app/a_propos.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__."/templates/header.php";
+// include __DIR__."/templates/header.php";
+?>
+
+<h1>A propos</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia modi necessitatibus debitis laborum asperiores eveniet eius obcaecati? Voluptates amet voluptas earum, consectetur et deserunt vel minima, repellat nemo incidunt expedita?</p>
+
+<?php require_once __DIR__."/templates/footer.php"; ?>
---
+++ app/lib/article.php
@@ -0,0 +1,9 @@
+<?php 
+
+  $articles = [
+    ["title" => "Php VS Python", "content" => "Truc !", "image" => "1-php-vs-python.jpg"],
+    ["title" => "React VS React-native", "content" => "Truc !", "image" => "2-react-vs-react-native.jpg"],
+    ["title" => "Les meilleurs outils DevOps ", "content" => "Truc !", "image" => "3-devops.png"],
+  ];
+
+  
