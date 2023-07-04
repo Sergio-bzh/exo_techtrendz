48fdf6cb6261cca165108b7f90a6de5ff94b6314
sergio NUNEZ <sergio.nunez@laposte.net>
2023-07-05 00:01:39 +0200

    STRUCTURE : add PHP menu dynamization + mainMenu array

--- app/templates/article_part.php
+++ app/templates/article_part.php
@@ -1,10 +1,10 @@
 <div class="col-md-4 my-2">
     <div class="card">
-        <img src="uploads/articles/<?php echo $article["image"]?>" class="card-img-top" alt="PHP vs Python">
+        <img src="uploads/articles/<?php echo $article["image"]?>" class="card-img-top" alt="<?=$article["title"]?>">
         <div class="card-body">
           <h5 class="card-title"><?=$article["title"]?></h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href=" #" class="btn btn-primary">Go somewhere</a>
+          <a href="actualites.php" class="btn btn-primary">Voir toutes les actus</a>
         </div>
     </div>
 </div>
