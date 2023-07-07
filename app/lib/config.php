a077599357f45cfadc6e728b3c2416c33b083db8
sergio NUNEZ <sergio.nunez@laposte.net>
2023-07-07 12:26:17 +0200

    STRUCTURE : after remote repo add config files to work with DB

---
+++ app/lib/config.php
@@ -0,0 +1,8 @@
+<?php 
+
+define("SERVER_NAME", "localhost");
+define("MYSQL_DATABASE", "myapp");
+define("MYSQL_USER", "myappuser");
+define("MYSQL_PASSWORD", "myapppassword");
+define("MYSQL_ROOT_PASSWORD", "example");
+
